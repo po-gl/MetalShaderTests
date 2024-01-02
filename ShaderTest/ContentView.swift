e6aa597c51a18ee6b4438eb4ff7bcda4550c7c30
po-gl <porterglines@gmail.com>
2024-01-02 02:11:34 -0700

    Initial Commit

---
+++ ShaderTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShaderTest
+//
+//  Created by Porter Glines on 1/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShaderTest/ShaderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaderTestApp.swift
+//  ShaderTest
+//
+//  Created by Porter Glines on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ShaderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
