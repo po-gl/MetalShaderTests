6969add75bc736f3abc1a85813dcdf5f589e256c
po-gl <porterglines@gmail.com>
2024-01-17 14:53:18 -0700

    Add pick gradient implementation

--- ShaderTest/Tests/PickGradient.metal
+++ ShaderTest/Tests/PickGradient.metal
@@ -9,11 +9,49 @@
 
 using namespace metal;
 
-[[ stitchable ]] half4 pickGradient(float2 pos, half4 color, float4 boundingRect, float size, half4 newColor) {
+extern float random(float2 st);
+
+[[ stitchable ]] half4 pickGradient(float2 pos, half4 existingColor, float4 boundingRect,
+                                    float zoom, half4 newColor, float top, float bottom) {
     float2 uv = pos / boundingRect.zw;
-//    uint2 posInChecks = uint2(uv.x / size, uv.y / size);
-    uint2 posInChecks = uint2(pos.x / size, pos.y / size);
-    bool isColor = (posInChecks.x ^ posInChecks.y) & 1;
-    newColor.a *= uv.y;
-    return isColor ? newColor : half4(0.0, 0.0, 0.0, 0.0);
+    uv.y = 1.0 - uv.y;
+//    uv.x += t * 0.05;
+    
+    uv.x *= boundingRect.z / boundingRect.w;
+    uv *= zoom;
+    uv.x -= zoom/2;
+    
+    float2 ipos = floor(uv);
+    
+    top = 1.0 - top;
+    bottom = 1.0 - bottom;
+    
+    float r = random(ipos);
+    r = step(smoothstep(top, bottom, 1.0 - ipos.y / zoom), r);
+    half3 color = half3(r);
+    
+    return half4(color, existingColor.a);
+}
+
+[[ stitchable ]] half4 pickGradientWithGradient(float2 pos, half4 existingColor, float4 boundingRect, half4 glow) {
+    float zoom = 60.0;
+    float2 uv = pos / boundingRect.zw;
+    uv.y = 1.0 - uv.y;
+    uv.x *= boundingRect.z / boundingRect.w;
+    uv *= zoom;
+    
+    float2 ipos = floor(uv);
+    
+    float r = random(ipos);
+    r = step(smoothstep(0.1, 0.8, 1.0 - ipos.y / zoom), r);
+    half4 color = half4(0.0, 0.0, 0.0, 1.0 - r);
+    
+    // Mix in gradient
+    glow.a = 1.0 - ((uv.y / zoom) + 0.2);
+    color = mix(color, glow, 1.0 - color.a);
+    
+    // Compositor expects premultiplied colors for alpha blending
+    color = half4(color.rgb * color.a, color.a);
+    
+    return color;
 }
--- ShaderTest/Tests/PickGradientTest.swift
+++ ShaderTest/Tests/PickGradientTest.swift
@@ -8,27 +8,193 @@
 import SwiftUI
 
 struct PickGradientTest: View {
-    @State var size = 10.0
+    var views : [Test] = [
+        Test(view: AnyView(BasicPickGradient()), title: "Basic"),
+        Test(view: AnyView(ColorEffectPickGradient()), title: "Recreated"),
+    ]
+
+    @State var selectedViewIdx = 1
+    var selectedViewIdxBinding: Binding<Int> {
+        Binding(get: { selectedViewIdx },
+                set: { val in withAnimation { selectedViewIdx = val }})
+    }
+
     var body: some View {
-        VStack(spacing: 30) {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(width: 300, height: 200)
-                .colorEffect(ShaderLibrary.pickGradient(.boundingRect, .float(size), .color(.mint)))
-            
-            GroupBox {
-                HStack {
-                    Text("Size:")
-                    Spacer()
-                    Text(String(format: "%.3f", size))
-                        .monospacedDigit()
+        VStack {
+            Picker("Pick a Shapes Shader", selection: selectedViewIdxBinding) {
+                ForEach(Array(views.enumerated()), id: \.offset) { i, view in
+                    Text(view.title).tag(i)
                 }
-                Slider(value: $size, in: 0.001...50)
             }
+            .pickerStyle(.segmented)
+            .padding()
+            
+            let selected = views[selectedViewIdx]
+            VStack {
+                Text(selected.title)
+                    .font(.title)
+                    .fontDesign(.serif)
+                selected.view
+            }
+            .id(selectedViewIdx)
+            .transition(BlurReplaceTransition(configuration: .downUp))
+            Spacer()
         }
         .padding()
     }
 }
+    
+struct BasicPickGradient: View {
+    let t0 = Date()
+    @State var zoom = 60.0
+    @State var top = 0.8
+    @State var bottom = 0.2;
+    
+    func animateZoom(delta: CGFloat) {
+        let tick = delta > 0 ? 1.0 : -1.0
+        Task { @MainActor in
+            for _ in 0..<abs(Int(delta)) {
+                try? await Task.sleep(for: .milliseconds(16))
+                zoom += tick;
+            }
+        }
+    }
+    func animateTop(delta: CGFloat, steps: Int) {
+        let tick = delta / CGFloat(steps)
+        
+        Task { @MainActor in
+            for _ in 0..<steps {
+                try? await Task.sleep(for: .milliseconds(16))
+                top += tick;
+            }
+        }
+    }
+    func animateBottom(delta: CGFloat, steps: Int) {
+        let tick = delta / CGFloat(steps)
+        
+        Task { @MainActor in
+            for _ in 0..<steps {
+                try? await Task.sleep(for: .milliseconds(16))
+                bottom += tick;
+            }
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 200)
+                .colorEffect(ShaderLibrary.pickGradient(.boundingRect,
+                                                        .float(zoom),
+                                                        .color(.mint),
+                                                        .float(top),
+                                                        .float(bottom)))
+                .border(.blue)
+                .allowsHitTesting(false)
+                .padding()
+            
+            GroupBox {
+                HStack {
+                    Text("Animate")
+                    Spacer()
+                    Button("Press") {
+                        // I like the animation of bottom the most
+//                        let zoomDelta = 50.0
+//                        let topDelta = 0.2
+                        let bottomDelta = 0.3
+//                        animateZoom(delta: zoomDelta)
+//                        animateTop(delta: topDelta, steps: Int(zoomDelta))
+                        animateBottom(delta: bottomDelta, steps: 50)
+                        Task {
+                            try? await Task.sleep(for: .seconds(2))
+//                            animateZoom(delta: -zoomDelta)
+//                            animateTop(delta: -topDelta, steps: Int(zoomDelta))
+                            animateBottom(delta: -bottomDelta, steps: 50)
+                        }
+                        
+                        // Animations through Transactions appears to not work with metal shaders
+//                        withAnimation {
+//                            zoom += zoomDelta
+//                        }
+//                        Task { @MainActor in
+//                            try? await Task.sleep(for: .seconds(3.0))
+//                            withAnimation {
+//                                zoom -= zoomDelta
+//                            }
+//                        }
+                    }
+                }
+            }
+            
+            GroupBox {
+                HStack {
+                    Text("Zoom:")
+                    Spacer()
+                    Text(String(format: "%.3f", zoom))
+                        .monospacedDigit()
+                }
+                Slider(value: $zoom, in: 0.001...150)
+            }
+            
+            GroupBox {
+                HStack {
+                    Text("Top:")
+                    Spacer()
+                    Text(String(format: "%.3f", top))
+                        .monospacedDigit()
+                }
+                Slider(value: $top, in: 0.01...1.0)
+            }
+            
+            GroupBox {
+                HStack {
+                    Text("Bottom:")
+                    Spacer()
+                    Text(String(format: "%.3f", bottom))
+                        .monospacedDigit()
+                }
+                Slider(value: $bottom, in: 0.01...1.0)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct ColorEffectPickGradient: View {
+    @Environment(\.colorScheme) var colorScheme
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(colorScheme == .dark ? .white : .white)
+                    .frame(width: 350, height: 350)
+                    .border(.blue)
+                HStack {
+                    Rectangle()
+                        .frame(width: 40, height: 40)
+                    Rectangle()
+                        .fill(.indigo)
+                        .frame(width: 40, height: 40)
+                }
+                .offset(y: -150)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 300, height: 300)
+                    .colorEffect(ShaderLibrary.pickGradientWithGradient(.boundingRect,
+                                                                        .color(.pink)))
+                    .allowsHitTesting(false)
+                    .border(.green)
+            }
+            .padding(.bottom)
+            
+            Text("PRE-MULTIPLIED ALPHA BLENDING.\nIt isn't well documented as a requirement by Apple, but the compositing layer expects pre-multiplied alpha blended colors.\nOtherwise two rectangles have their colors shifted. This appears to be a Metal bug where alpha values are not properly respected in .colorEffect(:).")
+                .fontDesign(.rounded)
+                .padding()
+        }
+    }
+}
 
 #Preview {
-    PickGradientTest()
+    ScrollView {
+        PickGradientTest()
+    }
 }
